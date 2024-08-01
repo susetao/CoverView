25e14f6968c08f11bae74394ea8a1e88ca2ebdde
susetao <susetaolei@gmail.com>
2024-08-01 19:26:26 +0800

    Update postcss.config.js更新中文注释

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
 module.exports = {
- 
+  // 定义了Webpack的插件配置
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    tailwindcss: {}, // 配置Tailwind CSS插件
+    autoprefixer: {}, // 配置Autoprefixer插件
   },
-}
+};
+
+// 这个配置文件是Webpack的配置文件的一部分，用于配置项目中的插件
+// Tailwind CSS插件用于在项目中使用Tailwind CSS，一个功能类优先的CSS框架
+// Autoprefixer插件用于自动添加CSS前缀，以兼容不同浏览器的CSS样式
